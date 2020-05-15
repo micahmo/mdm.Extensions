086f23486e64067a5e3517fe8510b32211153b86
Micah Morrison <micahmorrison95@gmail.com>
2020-05-15 19:25:04 -0400

    First commit, add IsNull and IsNotNull object extensions

---
+++ mdm.Extensions/mdm.Extensions/ObjectExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace mdm.Extensions
+{
+    /// <summary>
+    /// Defines a set of extensions methods that can be used on any <see cref="object"/>.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Returns <see langword="true"/> if the given <paramref name="object"/> is equal to <see langword="null"/>
+        /// (according to the <see langword="is"/> operator), otherwise <see langword="false"/>.
+        /// </summary>
+        public static bool IsNull(object @object)
+        {
+            return @object is null;
+        }
+
+        /// <summary>
+        /// Returns <see langword="true"/> if the given <paramref name="object"/> is not equal to <see langword="null"/>
+        /// (according to the <see langword="is"/> operator), otherwise <see langword="false"/>.
+        /// </summary>
+        public static bool IsNotNull(object @object)
+        {
+            return @object is null == false;
+        }
+    }
+}
