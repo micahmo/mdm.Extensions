3565d0fa57d0a93799ea546aa13dd83b6c794a4f
Micah Morrison <micah.morrison@jci.com>
2020-07-22 11:40:57 -0400

    Add IDictionary extension

--- mdm.Extensions/ObjectExtensions.cs
+++ mdm.Extensions/ObjectExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace mdm.Extensions
@@ -77,4 +78,32 @@ namespace mdm.Extensions
         }
     }
 
+    /// <summary>
+    /// Defines a set of extensions on <see cref="IDictionary{TKey, TValue}"/>.
+    /// </summary>
+    public static class IDictionaryExtensions
+    {
+        /// <summary>
+        /// Given a <paramref name="dictionary"/> with an <see cref="ICollection{TValue}"/> value type, checks if the given
+        /// <paramref name="key"/>'s value exists. If not, a new <see cref="List{TValue}"/> is created for the given <paramref name="key"/> with one element, the given <paramref name="value"/>.
+        /// If it already exists, the given <paramref name="value"/> is added to <paramref name="key"/>'s value collection.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>
+        public static void AddToCollection<TKey, TCollection, TValue>(this IDictionary<TKey, TCollection> dictionary, TKey key, TValue value) where TCollection : ICollection<TValue>, new()
+        {
+            if (dictionary is null)
+            {
+                throw new ArgumentNullException(nameof(dictionary));
+            }
+
+            if (dictionary.TryGetValue(key, out var collection))
+            {
+                collection.Add(value);
+            }
+            else
+            {
+                dictionary[key] = new TCollection { value };
+            }
+        }
+    }
 }
