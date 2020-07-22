f07e9b0831f0f9fe387a46c71173a681d508bc99
Micah Morrison <micah.morrison@jci.com>
2020-07-22 11:57:10 -0400

    Include build step in NuGet publishing script

--- mdm.Extensions/publish_nuget.ps1
+++ mdm.Extensions/publish_nuget.ps1
@@ -1,3 +1,4 @@
 remove-item bin\Release\*.nupkg
+dotnet build --configuration Release
 dotnet pack --configuration Release
 nuget push bin\Release\*.nupkg -Source https://api.nuget.org/v3/index.json
